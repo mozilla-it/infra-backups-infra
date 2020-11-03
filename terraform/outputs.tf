dcfe4f7f64c3bdda8c25e974efed83b0f0fac930
limed <limed@mozilla.com>
2020-11-02 16:13:04 -0800

    Added outputs and added precommit

---
+++ terraform/outputs.tf
@@ -0,0 +1,12 @@
+
+output "borg-mdc1-backups-iam_user" {
+  value = module.borg-mdc1-backups.iam_user
+}
+
+output "borg-mdc1-backups-iam_access_key" {
+  value = module.borg-mdc1-backups.iam_access_key
+}
+
+output "borg-mdc1-backups-iam_access_secret_key" {
+  value = module.borg-mdc1-backups.iam_access_secret_key
+}
